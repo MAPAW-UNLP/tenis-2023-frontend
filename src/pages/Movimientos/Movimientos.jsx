6791ffb93edd7c5e36d10273fac61b0ff09c4365
Juan Cruz <juan.cruz.gutierrez.saravia@gmail.com>
2023-10-29 22:23:32 -0300

    Agrego redireccion

--- src/pages/Movimientos/Movimientos.jsx
+++ src/pages/Movimientos/Movimientos.jsx
@@ -1,13 +1,17 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
 
 //components
 import NavBar from '../Navbar/NavBar'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faDollarSign, faUpLong, faDownLong, faScaleBalanced } from '@fortawesome/free-solid-svg-icons'
 import '../../styles/movimientos.css'
+
 export const Movimientos = ({ setSesion }) => {
-
-
+  const navigate = useNavigate()
+  const handleRedirect = (link) => {
+    navigate(link)
+  }
   return (
     <>
       <NavBar title={'Movimientos'} setSesion={setSesion} />
@@ -15,7 +19,7 @@ export const Movimientos = ({ setSesion }) => {
         width: '50%', margin: '0 auto', display: 'flex', flexWrap: 'wrap', justifyContent: 'center',
         marginTop: '2em', marginBottom: '2em'
       }}>
-        <div className="card cobros" style={{ marginRight: '1em' }}>
+        <div className="card cobros" style={{ marginRight: '1em' }} onClick={() => handleRedirect('../cobros')}>
           <div className="image" style={{ backgroundColor: '#75ebb0', position: 'relative' }}>
             <div style={{ fontSize: '5em', color: '#5d5d5d' }}>
               <FontAwesomeIcon icon={faDollarSign} style={{ position: 'absolute', left: '32%', top: '0', bottom: '0', margin: 'auto' }} />
@@ -23,12 +27,9 @@ export const Movimientos = ({ setSesion }) => {
             </div>
           </div>
           <div className="content">
-            <a href="#">
-              <span className="title" style={{ color: '#75ebb0' }}>
-                Cobros
-              </span>
-            </a>
-
+            <span className="title" style={{ color: '#75ebb0' }}>
+              Cobros
+            </span>
             <p className="desc">
               Movimiento de ingreso de dinero
             </p>
@@ -58,7 +59,7 @@ export const Movimientos = ({ setSesion }) => {
         <div className="card balance" style={{ marginTop: '1.2em', width: '38em', maxWidth: 'none' }}>
           <div className="image" style={{ backgroundColor: '#78a1ca', position: 'relative' }}>
             <div style={{ fontSize: '5em', color: '#5d5d5d' }}>
-              <FontAwesomeIcon icon={faScaleBalanced} style={{ position: 'absolute', left: '0', right:'0', top: '0', bottom: '0', margin: 'auto' }} />
+              <FontAwesomeIcon icon={faScaleBalanced} style={{ position: 'absolute', left: '0', right: '0', top: '0', bottom: '0', margin: 'auto' }} />
             </div>
           </div>
           <div className="content">

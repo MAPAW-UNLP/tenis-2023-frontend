b3f93ef6da10d080d4d03efb1f8b3f987ceb7a85
Juan Cruz <juan.cruz.gutierrez.saravia@gmail.com>
2023-11-16 02:28:03 -0300

    Agrego nombre en tablas de movimiento y agrego columna en balance general. Agrego spinner para esperar la confirmacion de carga de movimientos

--- src/components/Movimiento/MovimientoTable.jsx
+++ src/components/Movimiento/MovimientoTable.jsx
@@ -3,8 +3,9 @@ import { Fragment, useState, useEffect } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
 import InputComponent from '../Utils/InputComponent';
+import { CoverSpinner } from '../CoverSpinner';
 
-export const MovimientoTable = ({ movimientos }) => {
+export const MovimientoTable = ({ movimientos, loadingFetch }) => {
 
   const [movimientosFiltrados, setMovimientosFiltrados] = useState(movimientos)
 
@@ -24,41 +25,51 @@ export const MovimientoTable = ({ movimientos }) => {
   }, [movimientos]);
 
   return (
-    <div className="container" style={{
-      backgroundColor: 'white', paddingLeft: '5px', paddingRight: '5px', maxHeight: '60vh',
-      overflowY: 'scroll'
-    }}>
-      <table className="movimiento-table" style={{ marginBottom: '5px', position: 'relative' }}>
-        <thead className='table-head' style={{ position: 'sticky', top: '0' }}>
-          <tr>
-            <th>Día</th>
-            <th>Concepto</th>
-            <th style={{ textAlign: 'center' }}>Descripción</th>
-            <th>Monto</th>
-            <th style={{ width: '15em' }}>
-              <div className="movimiento-searchbar">
-                <FontAwesomeIcon className="movimiento-magnify-icon" icon={faMagnifyingGlass} />
-                <InputComponent type={'text'} placeholder={'Buscar por descripcion'} onChangeFuncion={handleSearchMovimiento} />
-              </div>
-            </th>
-          </tr>
-        </thead>
-        <tbody>
-          {movimientosFiltrados.map((movimiento) => (
-            <Fragment key={movimiento.id}>
-              <tr style={{ height: '8px' }} />
-              <tr className='table-row'>
-                <td>{movimiento.fecha}</td>
-                <td>{movimiento.concepto_desc}</td>
-                <td>{movimiento.descripcion}</td>
-                <td className='monto-td'>${movimiento.monto}</td>
-                <td></td>
-              </tr>
-            </Fragment>
-          ))
-          }
-        </tbody>
-      </table>
-    </div>
+    <>
+      <div className="container" style={{
+        backgroundColor: 'white', paddingLeft: '5px', paddingRight: '5px', maxHeight: '60vh',
+        overflowY: 'scroll'
+      }}>
+        <CoverSpinner loadingFetch={loadingFetch} />
+        <table className="movimiento-table" style={{ marginBottom: '5px', position: 'relative' }}>
+          <thead className='table-head' style={{ position: 'sticky', top: '0' }}>
+            <tr>
+              <th>Día</th>
+              <th>Concepto</th>
+              <th style={{ textAlign: 'center' }}>Descripción</th>
+              <th>Monto</th>
+              <th style={{ width: '15em' }}>
+                <div className="movimiento-searchbar">
+                  <FontAwesomeIcon className="movimiento-magnify-icon" icon={faMagnifyingGlass} />
+                  <InputComponent type={'text'} placeholder={'Buscar por descripcion'} onChangeFuncion={handleSearchMovimiento} />
+                </div>
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+            {movimientosFiltrados.map((movimiento) => (
+              <Fragment key={movimiento.id}>
+                <tr style={{ height: '8px' }} />
+                <tr className='table-row'>
+                  <td>{movimiento.fecha}</td>
+                  <td>{movimiento.concepto_desc}</td>
+                  {movimiento.idAlumno
+                    ? <td><span style={{ fontWeight: 'bold', fontSize: '1.1em' }}>{movimiento.nombreAlumno}</span> <br /> {movimiento.descripcion}</td>
+                    : (movimiento.idProfesor
+                      ? <td><span style={{ fontWeight: 'bold', fontSize: '1.1em' }}>{movimiento.nomProfesor}</span> <br /> {movimiento.descripcion}</td>
+                      : <td>{movimiento.descripcion}</td>
+                    )
+                  }
+                  {/* <td>{movimiento.descripcion}</td> */}
+                  <td className='monto-td'>${movimiento.monto}</td>
+                  <td></td>
+                </tr>
+              </Fragment>
+            ))
+            }
+          </tbody>
+        </table>
+      </div>
+    </>
   )
 }
